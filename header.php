170f212ebb81c8d5373aefd4abc62b4b5e1f235d
Baza58 <bazout.p@gmail.com>
2015-05-20 21:42:25 +0200

    add description

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 	<head>
         <meta charset="utf-8">
         <title>Petr Bažout | Front-end web developer</title>
+        <meta name="description" content="Ahoj! Jmenuju se Petr Bažout a zajímám se o vývoj webu.</span> Mezi moje znalosti patří například HTML5, CSS3, JavaScript, jQuery, Wordpress. Pokud se zrovna nezabývám webem, tak s největší pravděpodobností sportuju nebo čtu."
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="assets/css/style.css" rel="stylesheet">
         <meta name="author" content="Petr Bažout">
