5a030dbbc6e61845b81b84bc7a3c4cf7f091e977
Baza58 <bazout.p@gmail.com>
2015-05-21 12:43:28 +0200

    add responsive images

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
                         <p>
                             Stránky věnované soukromému Karate-Do klubu Sakura Žatec. Stránky můžete navštívit na adrese <a href="http://sakurazatec.cz" class="work-section-link" target="_blank">sakurazatec.cz</a>
                         </p>
-                        <img src="assets/img/sakura-zatec-screenshot.jpg" alt="Sakura Žatec stránky screenshot">
+                        <img src="assets/img/sakura-zatec-screenshot.jpg" srcset="assets/img/sakura-zatec-screenshot.jpg 1x, assets/img/sakura-zatec-screenshot2x.jpg 2x" alt="Sakura Žatec stránky screenshot">
                     </div> 
                 </div>
             </div>
